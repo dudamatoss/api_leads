fcfbdc23e964fded8af6832fafafc772adcc89b7
dudamatoss <164113930+dudamatoss@users.noreply.github.com>
2025-08-01 15:16:51 -0300

    fix:add controller

--- bin/server.dart
+++ bin/server.dart
@@ -4,6 +4,6 @@ Future<void> main(List<String> args) async {
   final vaden = VadenApp();
   await vaden.setup();
   final server = await vaden.run(args);
-  print('Server listening on port ${server.port}');
+  print('Server listening on port \n http://localhost:${server.port}/docs');
 }
 
